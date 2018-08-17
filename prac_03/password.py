028a5c4a0ea622a551886ba6dc9052b90ebc62d4
Fletcher Doolan <fletcher.doolan@my.jcu.edu.au>
2018-08-17 14:39:49 +1000

    Add function for password checker

--- prac_03/password.py
+++ prac_03/password.py
@@ -1,9 +1,23 @@
 MIN_LENGTH = 8
 
-print("Please input a password")
-print("A password is only valid if it has more than {} characters".format(MIN_LENGTH))
-password = input(": ")
-while len(password) < MIN_LENGTH:
-    print("Invalid password")
+
+def main():
+    password = input_password(MIN_LENGTH)
+    print_password_hidden(password)
+
+
+def input_password(password):
+    print("Please input a password")
+    print("A password is only valid if it has more than {} characters".format(MIN_LENGTH))
     password = input(": ")
-print("*" * len(password))
+    while len(password) < MIN_LENGTH:
+        print("Invalid password")
+        password = input(": ")
+    return password
+
+
+def print_password_hidden(password):
+    print("*" * len(password))
+
+
+main()
