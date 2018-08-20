2e7494ba45dba8bcad03bd9ee30eafe6f34bea1e
Fletcher Doolan <fletcher.doolan@my.jcu.edu.au>
2018-08-20 20:57:31 +1000

    Edit broken_score2

--- prac_03/broken_score2.py
+++ prac_03/broken_score2.py
@@ -2,28 +2,19 @@ def main():
     """Get an input of a score and display its value"""
     score = float(input("Enter score: "))
     result = score_check(score)
-    if result == 1:
-        print("Invalid score")
-    elif result == 2:
-        print("Your score is excellent")
-    elif result == 3:
-        print("Your score is a pass")
-    elif result == 4:
-        print("Your score is bad")
+    print(score_check(score))
 
 
 def score_check(score):
     """Determine type of result"""
-    result = 0
     if score < 0 or score > 100:
-        result += 1
+        return "Invalid score"
     elif score >= 90:
-        result += 2
+        return "Excellent score"
     elif score >= 50:
-        result += 3
+        return "Pass score"
     else:
-        result += 4
-    return result
+        return "Bad score"
 
 
 main()
