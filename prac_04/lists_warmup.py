0738fcdc48cc955f0442aaa324c2afcab499343d
Fletcher Doolan <fletcher.doolan@my.jcu.edu.au>
2018-08-21 13:24:30 +1000

    Add unfinished lists

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,13 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+"""
+numbers[0] - 3
+numbers[-1] - 2
+numbers[3] - 1
+numbers[:-1] - 3,1,4,1,5,9,2
+numbers[3:4] - 1,5
+5 in numbers - 9
+7 in numbers - IndexError
+"3" in numbers - TypeError
+numbers + [6, 5, 3] - numbers = 3, 1, 4, 1, 5, 9, 2, 6, 5, 3
+"""
