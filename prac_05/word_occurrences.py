5065e8833166f07eb44a0a16cf2e9d62ed3273ea
Fletcher Doolan <fletcher.doolan@my.jcu.edu.au>
2018-09-02 21:07:22 +1000

    Finish word_occurrences

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,8 +1,11 @@
-words = {}
+word_to_frequency = {}
 sentence = input("Enter a sentence: ")
 sentence_set = sentence.split(" ")
 for word in sentence_set:
-    word_frequency = words.get(word, 0)
-    words[word] = word_frequency + 1
-print(sentence_set)
-print(words)
+    frequency = word_to_frequency.get(word, 0)
+    word_to_frequency[word] = frequency + 1
+words = list(word_to_frequency.keys())
+words.sort()
+longest_word = max(len(word) for word in words)
+for word in words:
+    print("{:{}} : {}".format(word, longest_word, word_to_frequency[word]))
