8367ceeb71247290ca8657221ac671575c9422c9
Fletcher Doolan <fletcher.doolan@my.jcu.edu.au>
2018-09-04 15:29:44 +1000

    Add prac_06

---
+++ prac_06/languages.py
@@ -0,0 +1,11 @@
+from prac_06.programming_language import ProgrammingLanguage
+
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1991)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
+coders = [ruby, python, visual_basic]
+print("The dynamically typed languages are:")
+for coder in coders:
+    if coder.typing:
+        print("{}".format(coder.name))
