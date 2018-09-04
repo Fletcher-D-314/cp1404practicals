86980cb7a0c0e92047b253fb8b9cc74fe704c936
Fletcher Doolan <fletcher.doolan@my.jcu.edu.au>
2018-09-04 15:36:12 +1000

    Add add docstrings and change for loop

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,11 +1,14 @@
+"""
+Language
+"""
+
 from prac_06.programming_language import ProgrammingLanguage
 
 ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1991)
 python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
 visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-print(python)
 coders = [ruby, python, visual_basic]
 print("The dynamically typed languages are:")
 for coder in coders:
-    if coder.typing:
+    if coder.is_dynamic():
         print("{}".format(coder.name))
