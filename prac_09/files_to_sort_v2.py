b26f799631dbab9687c56a1932234b9c89425ba5
Fletcher Doolan <fletcher.doolan@my.jcu.edu.au>
2018-10-23 13:06:56 +1000

    Add files to sort v2 with same capabilies as v1

--- prac_09/files_to_sort_v2.py
+++ prac_09/files_to_sort_v2.py
@@ -3,12 +3,14 @@ import shutil
 
 DIRECTORY = "C:/Users/choos/OneDrive/Desktop/Pycharm Projects/cp1404practicals/prac_09"
 
+
 def main():
     os.chdir('FilesToSort')
     extensions = []
     get_extensions(extensions)
-    # make_directory(extensions)
-    # move_files(extensions)
+    make_directory(extensions)
+    move_files(extensions)
+
 
 def get_extensions(extensions):
     for filename in os.listdir('.'):
@@ -16,3 +18,33 @@ def get_extensions(extensions):
         extension = file[-1]
         if extension not in extensions:
             extensions.append(extension)
+
+
+def make_directory(extensions):
+    for extension in extensions:
+        try:
+            os.mkdir("{}/FilesToSort/{}".format(DIRECTORY, extension))
+        except FileExistsError:
+            pass
+
+
+def handle_user_input(extensions):
+    choices = []
+    for extension in extensions:
+        choice = input("What category would you like to sort {} files into? ".format(extension))
+        choices.append(choice)
+    return choices
+
+
+def move_files(extensions):
+    choices = handle_user_input(extensions)
+    for choice in choices:
+        os.chdir("{}/FilesToSort".format(DIRECTORY))
+        for file in os.listdir("."):
+            if file not in extensions:
+                split = file.split(".")
+                if choice == ".{}".format(split[-1]):
+                    shutil.move(file, choice)
+
+
+main()
